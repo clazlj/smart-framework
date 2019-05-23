fb31f5a5025e3740d2f46e31358954082ed508b1
caoliang <caoliang@jiangduoduo.com>
2019-05-23 16:43:43 +0800

    CgLib动态代理。引用3.1版本，无需重新引入asm

---
+++ src/main/java/org/smart4j/framework/CglibProxy.java
@@ -0,0 +1,40 @@
+package org.smart4j.framework;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibProxy implements MethodInterceptor {
+
+    private static CglibProxy instance = new CglibProxy();
+
+    private CglibProxy() {
+    }
+
+    public static CglibProxy getInstance() {
+        return instance;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getProxy(Class<T> cls) {
+        return (T) Enhancer.create(cls, this);
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        before();
+        Object result = methodProxy.invokeSuper(o, objects);
+        after();
+        return result;
+    }
+
+    private void before() {
+        System.out.println("Before");
+    }
+
+    private void after() {
+        System.out.println("After");
+    }
+}
--- src/test/java/org/smart4j/frameworktest/service/RunDemo.java
+++ src/test/java/org/smart4j/frameworktest/service/RunDemo.java
@@ -1,5 +1,6 @@
 package org.smart4j.frameworktest.service;
 
+import org.smart4j.framework.CglibProxy;
 import org.smart4j.framework.DynamicProxy;
 import org.smart4j.frameworktest.service.impl.HelloImpl;
 import org.smart4j.frameworktest.service.impl.HelloProxy;
@@ -15,6 +16,11 @@ public class RunDemo {
         DynamicProxy dynamicProxy = new DynamicProxy(new HelloImpl());
         helloProxy = dynamicProxy.getProxy();
         helloProxy.say("Tom");
+
+        //CgLib动态代理
+        //CglibProxy cglibProxy = new CglibProxy();用单例代替
+        helloProxy = CglibProxy.getInstance().getProxy(HelloImpl.class);
+        helloProxy.say("Helen");
 
     }
 }
